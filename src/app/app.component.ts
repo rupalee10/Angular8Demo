512ad274276b8b1ee8264780341afcd4e4f66027
rupalee <lokhande.rupalee10@gmail.com>
2020-01-30 22:05:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  tit="dgdg";
 }
